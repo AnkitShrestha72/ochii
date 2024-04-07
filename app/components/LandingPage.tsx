503870c656693203a7a19440ef7e6b0f5f51adf4
Ankit Shrestha <shresthaankit72@gmail.com>
2024-04-07 13:39:53 +0545

    initail commit

---
+++ app/components/LandingPage.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const LandingPage = () => {
+  return (
+    <div className='w-full h-screen bg-zinc-900'>
+        <div className="textstructure pt-40 px-20">
+        <div className="masker">
+            <h1 className="uppercase text-9xl leading-none tracking-tighter font-medium">
+                We Create
+            </h1>
+        </div>
+        </div>
+    </div>
+  )
+}
+
+export default LandingPage
